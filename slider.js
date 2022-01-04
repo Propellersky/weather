cc0adb203f11e8a08db0cc872174b8d8a22859d1
Ghennadi Chirilenco <propellersky87@gmail.com>
2022-01-04 21:01:27 +0200

    refactoring

---
+++ slider.js
@@ -0,0 +1,25 @@
+let sliderLine = document.querySelector(".slider-line")
+
+let offset = 0;
+
+document.querySelector(".swiper-button-prev").addEventListener("click", function () {
+  offset = offset -= 300;
+  if (offset == 0) {
+    document.querySelector(".swiper-button-prev").classList.add("none")
+  } else if (offset > 0) {
+    document.querySelector(".swiper-button-next").classList.remove("none")
+  }
+  sliderLine.style.right = offset + "px";
+})
+
+document.querySelector(".swiper-button-next").addEventListener("click", function () {
+
+  offset = offset += 300;
+
+  if (offset == 300) {
+    document.querySelector(".swiper-button-prev").classList.remove("none")
+  } else if (offset == 1200) {
+    document.querySelector(".swiper-button-next").classList.add("none")
+  }
+  sliderLine.style.right = offset + "px";
+})
