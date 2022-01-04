f9b36a5ae8cd81630a07553a7f9c5bae04e1003c
Ghennadi Chirilenco <propellersky87@gmail.com>
2022-01-04 21:03:44 +0200

    refactoring

--- script.js
+++ script.js
@@ -29,38 +29,6 @@ let daysArrRu = ['Воскресенье', 'Понедельник', 'Вторн
 
 
 
-let sliderLine = document.querySelector(".slider-line")
-
-let offset = 0;
-
-document.querySelector(".swiper-button-prev").addEventListener("click", function () {
-  offset = offset -= 300;
-  if (offset == 0) {
-    document.querySelector(".swiper-button-prev").classList.add("none")
-  } else if (offset > 0) {
-    document.querySelector(".swiper-button-next").classList.remove("none")
-  }
-  sliderLine.style.right = offset + "px";
-})
-
-
-document.querySelector(".swiper-button-next").addEventListener("click", function () {
-
-  offset = offset += 300;
-
-  if (offset == 300) {
-    document.querySelector(".swiper-button-prev").classList.remove("none")
-  } else if (offset == 1200) {
-    document.querySelector(".swiper-button-next").classList.add("none")
-  }
-  sliderLine.style.right = offset + "px";
-})
-
-
-
-
-
-
 
 function createWeatherApp() {
 
@@ -80,7 +48,7 @@ function createWeatherApp() {
 
           /*-------------------------------current forecast --------------------------------------------*/
           createForecastForCurrentDay(data);
-        
+
           /*-------------------------------hourly forecast --------------------------------------------*/
           hourlyForecastForCurrentDay(data)
 
@@ -179,3 +147,8 @@ function createWeatherApp() {
   // }
 }
 createWeatherApp();
+
+
+
+
+
